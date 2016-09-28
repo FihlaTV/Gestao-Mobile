72a6f1837b447b668d49f3bf214051cfa978e698
Diego Alejandro Franco <diego.fc.1@hotmail.com>
2016-09-27 20:53:01 -0500

    ajustes minimos

--- app/src/main/java/com/gestao/udec/gestao_mobile/RegisterActivity.java
+++ app/src/main/java/com/gestao/udec/gestao_mobile/RegisterActivity.java
@@ -13,9 +13,9 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         final EditText etnombre1 = (EditText) findViewById(R.id.etNombre1);
-        final EditText etnombre2 = (EditText) findViewById(R.id.etNombre2);
+
         final EditText etapellido1 = (EditText) findViewById(R.id.etApellido1);
-        final EditText etapellido2 = (EditText) findViewById(R.id.etApellido2);
+
         final EditText etcorreo = (EditText) findViewById(R.id.etCorreo);
         final EditText etclave = (EditText) findViewById(R.id.etClave);
         final RadioButton rbestudiante = (RadioButton) findViewById(R.id.rbEstudiante);
