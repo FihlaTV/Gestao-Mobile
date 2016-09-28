23bc67142f70c5d0a1cc019b3a8377afe50d918d
Diego Alejandro Franco <diego.fc.1@hotmail.com>
2016-09-27 20:15:16 -0500

    interfaces linkeadas

--- app/src/main/java/com/gestao/udec/gestao_mobile/LoginActivity.java
+++ app/src/main/java/com/gestao/udec/gestao_mobile/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.gestao.udec.gestao_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etcorreo = (EditText) findViewById(R.id.etCorreo);
+        final EditText etclave = (EditText) findViewById(R.id.etClave);
+        final Button btnregistro = (Button) findViewById(R.id.btnIngresar);
+        final TextView etregistro = (TextView) findViewById(R.id.etRegistro);
+        etregistro.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/gestao/udec/gestao_mobile/RegisterActivity.java
+++ app/src/main/java/com/gestao/udec/gestao_mobile/RegisterActivity.java
@@ -2,6 +2,9 @@ package com.gestao.udec.gestao_mobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final EditText etnombre1 = (EditText) findViewById(R.id.etNombre1);
+        final EditText etnombre2 = (EditText) findViewById(R.id.etNombre2);
+        final EditText etapellido1 = (EditText) findViewById(R.id.etApellido1);
+        final EditText etapellido2 = (EditText) findViewById(R.id.etApellido2);
+        final EditText etcorreo = (EditText) findViewById(R.id.etCorreo);
+        final EditText etclave = (EditText) findViewById(R.id.etClave);
+        final RadioButton rbestudiante = (RadioButton) findViewById(R.id.rbEstudiante);
+        final RadioButton rbdocente = (RadioButton) findViewById(R.id.rbDocente);
+        final Button btnregistro = (Button) findViewById(R.id.btnRegistro);
     }
 }
