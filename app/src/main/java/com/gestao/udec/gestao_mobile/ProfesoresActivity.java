b1bed53330f35496c97838ac6e40a4a7e2640792
Diego Alejandro Franco <diego.fc.1@hotmail.com>
2016-10-06 20:51:11 -0500

    profesores full

--- app/src/main/java/com/gestao/udec/gestao_mobile/ProfesoresActivity.java
+++ app/src/main/java/com/gestao/udec/gestao_mobile/ProfesoresActivity.java
@@ -11,8 +11,12 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,14 +39,18 @@ import java.util.Map;
 public class ProfesoresActivity extends AppCompatActivity {
     String showteachers = "http://192.168.1.4/gestao/mobile/show_teachers.php";
     String showteacheree = "http://192.168.1.4/gestao/mobile/show_teachers_e.php";
+    String showteacherec = "http://192.168.1.4/gestao/mobile/select_latest_clases_e.php";
     ImageView ifacebook;
     ImageView itwiter;
     RequestQueue requestQueue;
     JSONArray jArray;
-    TextView tvnombrefull;
+    TextView tvnombrefull, tvcorreo, tvdescrip, tvtelefono;
     AutoCompleteTextView auto;
     String id_profesor = "";
-
+    TableLayout tlclase;
+    ScrollView sv;
+    TableRow trclase;
+    Button btConsultar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SessionManager sesion = new SessionManager(ProfesoresActivity.this);
@@ -52,15 +60,25 @@ public class ProfesoresActivity extends AppCompatActivity {
         ifacebook = (ImageView) findViewById(R.id.ifacebook);
         itwiter = (ImageView) findViewById(R.id.itwiter);
         auto = (AutoCompleteTextView) findViewById(R.id.actProfesor);
-        Button btConsultar = (Button) findViewById(R.id.btconsutar);
+        btConsultar = (Button) findViewById(R.id.btconsutar);
         tvnombrefull = (TextView) findViewById(R.id.tvnombrefull);
+         sv = (ScrollView)findViewById(R.id.sv);
+        tvcorreo = (TextView) findViewById(R.id.tvcorreo);
+        tvdescrip = (TextView) findViewById(R.id.tvdescrip);
+        tvtelefono = (TextView) findViewById(R.id.tvtelefono);
+        tlclase = (TableLayout) findViewById(R.id.tlclases);
+        trclase = (TableRow)  findViewById(R.id.trclase);
         requestQueue = Volley.newRequestQueue(getApplicationContext());
         String font_path = "fonts/Ubuntu-C.ttf";
         Typeface TF = Typeface.createFromAsset(getAssets(),font_path);
         auto.setTypeface(TF);
+        tvcorreo.setTypeface(TF);
         tvnombrefull.setTypeface(TF);
+        btConsultar.setTypeface(TF);
+        tvdescrip.setTypeface(TF);
         itwiter.setVisibility(View.INVISIBLE);
         ifacebook.setVisibility(View.INVISIBLE);
+        tlclase.setVisibility(View.INVISIBLE);
         obtener_profesores();
 
 
@@ -105,6 +123,8 @@ public class ProfesoresActivity extends AppCompatActivity {
         btConsultar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                sv.scrollTo(0, btConsultar.getBottom());
                 InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
 
                 inputMethodManager.hideSoftInputFromWindow(auto.getWindowToken(), 0);
@@ -144,6 +164,8 @@ obtener_profesor();
 
 
     protected void obtener_profesor() {
+        while (tlclase.getChildCount() > 1)
+            tlclase.removeView(tlclase.getChildAt(tlclase.getChildCount() - 1));
         StringRequest request = new StringRequest(Request.Method.POST, showteacheree, new Response.Listener<String>() {
 
             @Override
@@ -156,10 +178,15 @@ obtener_profesor();
                     for (int i = 0; i < jArray.length(); i++) {
                         JSONObject profesor = jArray.getJSONObject(i);
                         itwiter.setTag(profesor.getString("twiter"));
+
                         ifacebook.setTag(profesor.getString("facebook"));
+
                         itwiter.setVisibility(View.VISIBLE);
                         ifacebook.setVisibility(View.VISIBLE);
                         tvnombrefull.setText(profesor.getString("nombre1")+" "+profesor.getString("nombre2")+" "+profesor.getString("apellido1")+" "+profesor.getString("apellido2"));
+                        tvdescrip.setText(profesor.getString("descripcion"));
+                        tvcorreo.setText(profesor.getString("email"));
+                        tvtelefono.setText(profesor.getString("telefono"));
                     }
 
 
@@ -182,6 +209,69 @@ obtener_profesor();
             }
         };
         requestQueue.add(request);
+
+
+        ////////////////////////
+        StringRequest requesttwo = new StringRequest(Request.Method.POST, showteacherec, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                tlclase.setVisibility(View.VISIBLE);
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray jArray = jsonResponse.getJSONArray("response");
+
+
+                    for (int i = 0; i < jArray.length(); i++) {
+                        JSONObject clases = jArray.getJSONObject(i);
+
+
+                        trclase = new TableRow(getApplicationContext());
+                        trclase.setId(100+i);
+
+                        TextView tvcol1 = new TextView(getApplicationContext());
+                        tvcol1.setId(200+i);
+
+                        tvcol1.setText(clases.getString("nombre_clase"));
+                        TextView tvcol2 = new TextView(getApplicationContext());
+                        tvcol2.setId(200+i);
+                        tvcol2.setText(clases.getString("nombre_aula"));
+                        TextView tvcol3 = new TextView(getApplicationContext());
+                        tvcol3.setId(200+i);
+                        tvcol3.setText(clases.getString("hora_inicio") + " - "+clases.getString("hora_final"));
+
+                        TextView tvcol5 = new TextView(getApplicationContext());
+                        tvcol5.setId(200+i);
+                        tvcol5.setText(clases.getString("fecha"));
+                        trclase.addView(tvcol1);
+                        trclase.addView(tvcol2);
+                        trclase.addView(tvcol3);
+
+
+                        trclase.addView(tvcol5);
+                        tlclase.addView(trclase);
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ProfesoresActivity.this, getResources().getString(R.string.errorConexion), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("id_profesor", id_profesor);
+
+                return parameters;
+            }
+        };
+        requestQueue.add(requesttwo);
+
     }
 
 
