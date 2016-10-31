928390fd3dd91cbc0a65820f0d4a25e2d3c7ffec
Diego Alejandro Franco <diego.fc.1@hotmail.com>
2016-10-31 15:49:24 -0500

    estilo escanear estudiantes

--- app/src/main/java/com/gestao/udec/gestao_mobile/EscanearActivity.java
+++ app/src/main/java/com/gestao/udec/gestao_mobile/EscanearActivity.java
@@ -3,11 +3,15 @@ package com.gestao.udec.gestao_mobile;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -31,14 +35,14 @@ import java.util.Map;
 
 public class EscanearActivity extends AppCompatActivity {
     TextView aulaNombre;
-    String observaciones="";
+    String observaciones="",clase="",profesor="",aula_c="",hora_iniciof="",hora_finalf="";
     RequestQueue requestQueue;
-    TextView tvfecha, tvhora, tvaula, tvvacio, tvclase_actual;
+    TextView tvfecha, tvhora, tvaula, tvclase_actual,tvclasess;
     String aula, hora, minuto;
     String scan = "http://192.168.1.66/gestao/mobile/escanear.php";
     String scan1 = "http://192.168.1.66/gestao/mobile/escanear_mis_clases.php";
     String scan2 = "http://192.168.1.66/gestao/mobile/escanear_mi_clase.php";
-
+    RelativeLayout rlclasem;
     TableLayout tlclase;
     ScrollView sv;
     SessionManager sesion;
@@ -55,8 +59,11 @@ public class EscanearActivity extends AppCompatActivity {
         tvhora = (TextView) findViewById(R.id.tvhora);
         tvfecha = (TextView) findViewById(R.id.tvfecha);
         tvaula = (TextView) findViewById(R.id.tvaula);
-        tvvacio= (TextView) findViewById(R.id.tvvacio);
+        tvclasess = (TextView) findViewById(R.id.tvclasess);
+        rlclasem = (RelativeLayout) findViewById(R.id.rlclasem);
+        rlclasem.setVisibility(View.INVISIBLE);
         tvclase_actual= (TextView) findViewById(R.id.tvclase_actual);
+
         tlclase = (TableLayout) findViewById(R.id.tlclases);
 
         trclase = (TableRow)  findViewById(R.id.trclase);
@@ -64,8 +71,8 @@ public class EscanearActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(EscanearActivity.this);
-                builder.setMessage(observaciones)
-                        .setTitle("OBSERVACIONES")
+                builder.setMessage("Clase: "+clase+"\n"+"Profesor: "+profesor+"\n"+"Sala: "+aula_c+"\n"+"Hora de inicio: "+hora_iniciof+"\n"+"Hora final:"+hora_finalf+"\n"+"Observaciones: "+observaciones)
+                        .setTitle(clase)
                         .setCancelable(false)
                         .setNeutralButton("Aceptar",
                                 new DialogInterface.OnClickListener() {
@@ -110,13 +117,12 @@ public class EscanearActivity extends AppCompatActivity {
                             JSONObject resumen = jArray.getJSONObject(i);
                             tvaula.setText(tvaula.getText()+": "+aula);
                             tvfecha.setText(tvfecha.getText()+": "+resumen.getString("fecha"));
-                            tvhora.setText(tvhora.getText()+": "+resumen.getString("hora"));
-                           hora = resumen.getString("hora");
-
+                            tvhora.setText(tvhora.getText()+": "+resumen.getString("hora")+":"+resumen.getString("minuto"));
+                            hora = resumen.getString("hora");
                             minuto = resumen.getString("minuto");
                         }
                     }else{
-                        Toast.makeText(EscanearActivity.this, getResources().getString(R.string.errorNoClases), Toast.LENGTH_LONG).show();
+
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -140,7 +146,7 @@ public class EscanearActivity extends AppCompatActivity {
 
     }
 
-    protected void traermisiguienteclase(final String id, String hora_inicio, String hora_final, final String nombre){
+    protected void traermisiguienteclase(final String id, final String hora_inicio, final String hora_final, final String nombre){
         requestQueue = Volley.newRequestQueue(getApplicationContext());
         StringRequest request1 = new StringRequest(Request.Method.POST, scan2, new Response.Listener<String>() {
 
@@ -151,8 +157,9 @@ public class EscanearActivity extends AppCompatActivity {
                     JSONObject jsonResponse = new JSONObject(response);
                     JSONArray jArray = jsonResponse.getJSONArray("response");
 
-                    if(jArray.length()>=0){
+                    if(jArray.length()>0){
                         String nombre1;
+
                         String apellido;
                         JSONObject miclase = jArray.getJSONObject(0);
                         if (miclase.getString("nombre1").toString().trim().equalsIgnoreCase("null")){
@@ -165,7 +172,30 @@ public class EscanearActivity extends AppCompatActivity {
                         }else{
                             apellido = miclase.getString("apellido1");
                         }
-                        tvclase_actual.setText(miclase.getString("materia")+" "+nombre1+" "+apellido+nombre);
+                        rlclasem.setVisibility(View.VISIBLE);
+                        tvclase_actual.setVisibility(View.VISIBLE);
+                        clase=miclase.getString("materia");
+                        profesor=nombre1+" "+apellido;
+                        aula_c=nombre;
+                        hora_iniciof=hora_inicio;
+                        hora_finalf=hora_final;
+                        int horaserver=Integer.parseInt(hora);
+                        int horabd=Integer.parseInt(hora_inicio);
+                        int horas_diferencia= horabd-horaserver;
+                        int minutos_diferencia = 60-Integer.parseInt(minuto);
+                        String horast;
+                        if (Integer.parseInt(hora)>=Integer.parseInt(hora_inicio)){
+                            horast = " ya mismo en "+nombre;
+                        }
+                        else{
+                            if(horas_diferencia--==0){
+                                horast = " en : "+String.valueOf(minutos_diferencia)+" minutos";
+                            }else{
+                                horast = " en : "+String.valueOf(horas_diferencia)+" horas "+String.valueOf(minutos_diferencia)+" minutos";
+                            }
+
+                        }
+                        tvclase_actual.setText(miclase.getString("materia")+horast);
                     }else{
                         tvclase_actual.setText("hoy no tienes ninguna clase!");
                     }
@@ -200,7 +230,9 @@ public class EscanearActivity extends AppCompatActivity {
                     JSONObject jsonResponse = new JSONObject(response);
                     JSONArray jArray = jsonResponse.getJSONArray("response");
 
-                    if(jArray.length()>=0){
+                    if(jArray.length()>0){
+                        rlclasem.setLayoutParams(new TableLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                                RelativeLayout.LayoutParams.WRAP_CONTENT, 1f));
                         JSONObject miclase = jArray.getJSONObject(0);
                         if (miclase.getString("observaciones").equalsIgnoreCase("null")){
                             observaciones="no hay observaciones";
@@ -208,10 +240,12 @@ public class EscanearActivity extends AppCompatActivity {
                             observaciones=miclase.getString("observaciones");
                         }
 
+
                             traermisiguienteclase(miclase.getString("id"),miclase.getString("hora_inicio"),miclase.getString("hora_final"),miclase.getString("nombre"));
 
                     }else{
-                        tvclase_actual.setText("hoy no tienes ninguna clase!");
+
+
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -246,55 +280,72 @@ public class EscanearActivity extends AppCompatActivity {
                     JSONArray jArray = jsonResponse.getJSONArray("response");
 
                     if(jArray.length()>0){
-
+                        tvclasess.setVisibility(View.VISIBLE);
                         for (int i = 0; i < jArray.length(); i++) {
                             JSONObject clase_en_aula = jArray.getJSONObject(i);
                             if(i==0){
+                               int horaserver = Integer.parseInt(hora);
 
-                            if (Integer.parseInt(hora) < Integer.parseInt(clase_en_aula.getString("hora_inicio"))){
-                                tvvacio.setVisibility(View.VISIBLE);
-                            }}
+                                int horabd= Integer.parseInt(clase_en_aula.getString("hora_inicio"));
+                                if (horabd>horaserver){
+                                    trclase = new TableRow(getApplicationContext());
+                                    trclase.setId(100+i+1);
+                                    trclase.setBackgroundResource(R.drawable.edittextstyle);
+                                    TextView tvcol8 = new TextView(getApplicationContext());
+                                    tvcol8.setId(200+i);
+                                    String font_path = "fonts/Ubuntu-C.ttf";
+                                    final Typeface TF = Typeface.createFromAsset(getAssets(), font_path);
+                                    tvcol8.setTypeface(TF);
+                                    Color.parseColor("#000000");
+                                    tvcol8.setTextColor(Color.parseColor("#000000"));
+                                    tvcol8.setText(" Vacio actualmente!");
+                                    trclase.addView(tvcol8);
+                                    LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                                    llp.setMargins(30, 30, 30, 30); // llp.setMargins(left, top, right, bottom);
+
+                                    trclase.setLayoutParams(llp);
+                                    tlclase.addView(trclase);
+
+                                }
+
+                           }
                             trclase = new TableRow(getApplicationContext());
+                            LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                            llp.setMargins(30, 30, 30, 30); // llp.setMargins(left, top, right, bottom);
+
+                                                      trclase.setLayoutParams(llp);
+                            trclase.setBackgroundResource(R.drawable.edittextstyle);
                             trclase.setId(100+i);
                             TextView tvcol1 = new TextView(getApplicationContext());
                             tvcol1.setId(200+i);
-
-                            tvcol1.setText(clase_en_aula.getString("nombre"));
-                            TextView tvcol2 = new TextView(getApplicationContext());
-                            tvcol2.setId(200+i);
-
+                            String font_path = "fonts/Ubuntu-C.ttf";
+                            final Typeface TF = Typeface.createFromAsset(getAssets(), font_path);
+                            tvcol1.setTypeface(TF);
+                            Color.parseColor("#000000");
+                            tvcol1.setTextColor(Color.parseColor("#000000"));
+                            String nombretemp;
                             if (clase_en_aula.getString("nombre1").toString().trim().equalsIgnoreCase("null")){
-                                tvcol2.setText("docente no encontrado!");
+                                nombretemp ="docente no encontrado!";
                             }
                             else{
-                                tvcol2.setText(clase_en_aula.getString("nombre1")+" "+clase_en_aula.getString("apellido1"));
+                                nombretemp =clase_en_aula.getString("nombre1")+" "+clase_en_aula.getString("apellido1");
                             }
-                            TextView tvcol4 = new TextView(getApplicationContext());
-                            tvcol4.setId(200+i);
+                            tvcol1.setText(" Clase: "+clase_en_aula.getString("nombre")+"\n"+" Docente: "+nombretemp+"\n"+" Hora inicio: "+clase_en_aula.getString("hora_inicio")+"\n"+" Hora final: "+clase_en_aula.getString("hora_final"));
 
-                            tvcol4.setText(clase_en_aula.getString("hora_inicio"));
-                            TextView tvcol5 = new TextView(getApplicationContext());
-                            tvcol5.setId(200+i);
-
-                            tvcol5.setText(clase_en_aula.getString("hora_final"));
-                            trclase.addView(tvcol1);
-                            trclase.addView(tvcol2);
-                            trclase.addView(tvcol4);
-                            trclase.addView(tvcol5);
-                            tlclase.addView(trclase);
                             if (Integer.parseInt(clase_en_aula.getString("hora_inicio"))<=Integer.parseInt(hora) && Integer.parseInt(clase_en_aula.getString("hora_final"))>Integer.parseInt(hora)){
-                                TextView tvcol3 = new TextView(getApplicationContext());
-                                tvcol3.setId(200+i);
-                                tvcol3.setText("actualmente");
-                                trclase.addView(tvcol3);
+                                tvcol1.setText(tvcol1.getText()+"\n"+" ACTUALMENTE");
                             }
+                            trclase.addView(tvcol1);
+                            tlclase.addView(trclase);
+
 
                         }
                     }else{
-                        Toast.makeText(EscanearActivity.this, getResources().getString(R.string.errorNoClases), Toast.LENGTH_LONG).show();
+                        Toast.makeText(EscanearActivity.this, "No se encontraron clases para esta aula", Toast.LENGTH_LONG).show();
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
+
                 }
             }
         }, new Response.ErrorListener() {
